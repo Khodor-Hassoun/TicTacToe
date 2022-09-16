ba596f5674234ebe54d7cc0f66872cdc45754b1e
Khodor Hassoun <khodorhassoun@outlook.com>
2022-09-16 20:58:01 +0300

    Hi this is going to be a long message but I think I am unable to continue due to the time constraints I am able to get if player 1 made a move but I am unable to do the calculations properly. I'm off to the other assignment but I'll try to get back to this monstrosity

--- app.js
+++ app.js
@@ -7,17 +7,37 @@ for(let square of squares){
     square.setAttribute('grid-area',`${i}`)
     i++;
 }
+const winValue = 2
+
+const winningConditions = [
+  [squares[0], squares[1], squares[2]],
+  [squares[3], squares[4], squares[5]],
+  [squares[6], squares[7], squares[8]],
+  [squares[0], squares[3],squares[6]],
+  [squares[1], squares[4], squares[7]],
+  [squares[2], squares[5], squares[8]],
+  [squares[0], squares[4], squares[8]],
+  [squares[2], squares[4], squares[6]]
+];
 
 let Player = true;
 
 for (let square of squares) {
   square.addEventListener("click", () => {
-    if (
-      !square.classList.contains("player1") &&
-      !square.classList.contains("player2")
-    ){
+    if (!square.classList.contains("player1") &&!square.classList.contains("player2")){
       if (Player) {
         square.classList.add("player1");
+        for(let winningCondition of winningConditions){
+          for(let winning of winningCondition){
+            let currentValue = 0
+            if(winning.classList.contains('player1')){
+              currentValue++
+              if(currentValue === winValue ){
+                console.log('player 1 won')
+              }
+            }
+          }
+        }
         Player = false;
       } else {
         square.classList.add("player2");
