78e339605f8b9178dffeb252cc0704a8d411d318
Khodor Hassoun <khodorhassoun@outlook.com>
2022-09-16 20:15:01 +0300

    Graphics working fine

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 const squares = document.querySelectorAll('.square')
+const player1 =document.querySelector('.player1-img')
+const player2 = document.querySelector('.player2-img')
+const image= document.createElement('img')
+let i=1
+for(let square of squares){
+    square.setAttribute('grid-area',`${i}`)
+    i++;
+}
 
-for(square of squares){
-    square.addEventListener('click', ()=>{
-        console.log('working')
-    })
+let Player = true;
+
+for (let square of squares) {
+  square.addEventListener("click", () => {
+    if (
+      !square.classList.contains("player1") &&
+      !square.classList.contains("player2")
+    ){
+      if (Player) {
+        square.classList.add("player1");
+        Player = false;
+      } else {
+        square.classList.add("player2");
+        Player = true
+      }
+    }
+  });
 }
