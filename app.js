7abd7af20f2302dd5c09beb20208007d87ffb662
Khodor Hassoun <khodorhassoun@outlook.com>
2022-09-23 11:18:34 +0300

    2 player game working

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const squares = document.querySelectorAll('.square')
 const player1 =document.querySelector('.player1-img')
 const player2 = document.querySelector('.player2-img')
 const image= document.createElement('img')
-let i=1
+let i=0
 for(let square of squares){
     square.setAttribute('grid-area',`${i}`)
     i++;
 }
 const winValue = 2
-
+let currentValue = 0
+// winning.classList.contains('player1')
 const winningConditions = [
   [squares[0], squares[1], squares[2]],
   [squares[3], squares[4], squares[5]],
@@ -27,20 +28,22 @@ for (let square of squares) {
     if (!square.classList.contains("player1") &&!square.classList.contains("player2")){
       if (Player) {
         square.classList.add("player1");
+        // winning conditions is an array of arrays. winning condition is a winning array
         for(let winningCondition of winningConditions){
-          for(let winning of winningCondition){
-            let currentValue = 0
-            if(winning.classList.contains('player1')){
-              currentValue++
-              if(currentValue === winValue ){
-                console.log('player 1 won')
-              }
-            }
+          // Enter each element of array
+          if(winningCondition[0].classList.contains('player1') && winningCondition[1].classList.contains('player1') && winningCondition[2].classList.contains('player1')){
+            console.log('Player 1 wins')
           }
         }
         Player = false;
       } else {
         square.classList.add("player2");
+        for(let winningCondition of winningConditions){
+          // Enter each element of array
+          if(winningCondition[0].classList.contains('player2') && winningCondition[1].classList.contains('player2') && winningCondition[2].classList.contains('player2')){
+            console.log('Player 2 wins')
+          }
+        }
         Player = true
       }
     }
